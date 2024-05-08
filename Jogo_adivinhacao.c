0d3ab67c295274ab7c606600620c71a1e42fd45d
Johannknak <163207827+Johannknak@users.noreply.github.com>
2024-05-08 01:14:49 -0300

    Add files via upload

---
+++ Jogo_adivinhacao.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int dificuldade, tentativas, numerosecreto, i, numero, pontosperdidos = 0;
+    float pontuacao;
+    srand(time(0));
+    numerosecreto = rand() % 100;
+    printf("BEM VINDO AO JOGO DE ADIVINHACAO!\nSelecione a dificuldade:\n (1) Facil\n (2) Medio\n (3) Dificil\n");
+    scanf("%d", &dificuldade);
+    if(dificuldade==1)
+        tentativas = 18;
+    if(dificuldade==2)
+        tentativas = 12;
+    if(dificuldade==3)
+        tentativas = 6;
+    for( i=1 ; i<=tentativas && numerosecreto!=numero ; i++ ){
+        printf("\nEssa eh sua tentativa numero %d\nDigite seu numero: ", i);
+        scanf("%d", &numero);
+        if(i==tentativas)
+            printf("\nSuas tentativas acabaram! PERDEU! O numero secreto era %d", numerosecreto);
+        else if(numero<numerosecreto)
+            printf("O numero secreto eh maior, tente novamente: \n");
+        else if(numero>numerosecreto)
+            printf("O numero secreto eh menor, tente novamente: \n");
+        else if(numero==numerosecreto)
+            printf("\nMUITO BEM!! VOCE ACERTOU!!");
+        pontosperdidos = pontosperdidos + abs(numero - numerosecreto);
+}
+    pontuacao = ((101-tentativas) * (1000-pontosperdidos) )/10000.0 ;
+    printf("Sua pontuacao eh %.2f", pontuacao);
+    return 0;
+}
